59e527224cf3c8e1893dc4f7812cad86a5817802
apitchayakeaw-blip <apitchaya.keaw@bumail.net>
2026-03-27 23:33:36 +0700

    add jump

---
+++ Assets/Scripts/Mashroom.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class Mashroom : MonoBehaviour
+{
+    public float Forced = 20;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("player hit");
+
+
+            Player player = collision.gameObject.GetComponent<Player>();
+            player.rb.AddForce(Vector2.up * Forced, ForceMode2D.Impulse);
+        }
+    }
+
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,50 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    public Rigidbody2D rb;
+    public float gravityMultiplier = 20f;
+
+    public float Speed = 10;
+
+    private InputAction jumpAction;
+    public float JumpForce = 0.5f;
+    bool isOnGround;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        jumpAction = InputSystem.actions.FindAction("Jump");
+
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+       Physics2D.gravity *= gravityMultiplier;
     }
 
     // Update is called once per frame
     void Update()
     {
+        var moveAction = InputSystem.actions.FindAction("Move");
+        var hInput = moveAction.ReadValue<Vector2>().x;
+        transform.Translate(hInput * Speed * Time.deltaTime * Vector3.right);
+
+        if (jumpAction.triggered && isOnGround == true)
+        {
+            rb.AddForce(JumpForce * Vector2.up , ForceMode2D.Impulse);
+            isOnGround = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
         
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
     }
 }
