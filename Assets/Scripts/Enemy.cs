f6a3863e0a169378e0451b00287ead04d9c40e64
apitchayakeaw-blip <apitchaya.keaw@bumail.net>
2026-03-28 00:54:56 +0700

    enemy walk

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,15 +2,34 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] private Transform pointA;
+    [SerializeField] private Transform pointB;
+    [SerializeField] private int movespeed;
+
+    private Transform pointerTransform;
+    private Vector2 targetPosition;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        pointerTransform = GetComponent<Transform>();
+        targetPosition = pointB.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        pointerTransform.position = Vector2.MoveTowards(pointerTransform.position, targetPosition, movespeed * Time.deltaTime);
+
+        if (Vector2.Distance(pointerTransform.position, pointA.position) < 0.1f)
+        {
+            targetPosition = pointB.position;
+
+        }
+        else if (Vector2.Distance(pointerTransform.position, pointB.position) < 0.1f)
+        {
+            targetPosition = pointA.position;
+
+        }
     }
 }
